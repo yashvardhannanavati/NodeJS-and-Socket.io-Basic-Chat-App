1301e25bd1abab8c48f85511cce5b1d174b77139
yashvardhannanavati <yashn@bu.edu>
2017-03-23 19:20:08 -0400

    Simple chat app on Localhost

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	//res.send('<h1>Hello world!</h1>');
+	res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', function(socket){
+	console.log('a user is connected');
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});
+});
+
+
+http.listen(3000, function(){
+console.log('listening on *:3000');
+});
